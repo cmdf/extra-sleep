288acfacaa2ab2a53b1e7e0a249a04adf118ccdf
Subhajit Sahu <wolfram77@gmail.com>
2016-07-11 00:15:40 +0530

    program: main comment added

--- cmd-sleep/Program.cs
+++ cmd-sleep/Program.cs
@@ -3,6 +3,13 @@ using System.Threading;
 
 namespace orez.osleep {
 	class Program {
+
+		// static method
+		/// <summary>
+		/// I am right here, but i don't know here.
+		/// : Watch Dogs 2 Trailer
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
 			oParams p = new oParams(args);
 			TimeSpan t = new TimeSpan(p.Days, p.Hours, p.Minutes, p.Seconds, p.Milliseconds);
